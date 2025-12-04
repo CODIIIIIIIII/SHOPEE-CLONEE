bacfc5137c59f80679c6a4e0c035b1829dfd4391
Jashley Denzel <denzelbolito24@gmail.com>
2025-12-05 04:35:06 +0800

    initial commit

--- Shopee-Clone/src/App.tsx
+++ Shopee-Clone/src/App.tsx
@@ -1,3 +1,27 @@
-// App.tsx
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import BuyerLandingPage from './features/buyer/BuyerLandingPage';
+import BuyerSignUp from './features/buyer/BuyerSignUp';
+import BuyerLogIn from './features/buyer/BuyerLogIn';
+import BuyerProductDetail from './features/buyer/BuyerProductDetail';
+import BuyerAccount from './features/buyer/BuyerAccount';
+import BuyerPurchase from './features/buyer/BuyerPurchase';
 
+const App: React.FC = () => {
+  console.log('App component rendering...');
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<BuyerLandingPage />} />
+        <Route path="/signup" element={<BuyerSignUp />} />
+        <Route path="/login" element={<BuyerLogIn />} />
+        <Route path="/account" element={<BuyerAccount />} />
+        <Route path="/purchase" element={<BuyerPurchase />} />
+        <Route path="/product/:id" element={<BuyerProductDetail />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
 
