dea3584ec6543beefaa756b24905122938258c98
CODIIIIIIIII <codinerarafael@gmail.com>
2025-12-05 04:21:39 +0800

    "initial commit"

--- Shopee-Clone/src/App.tsx
+++ Shopee-Clone/src/App.tsx
@@ -1,3 +1,22 @@
-// App.tsx
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import Login from './features/auth/components/Login';
+import SellerDashboard from './features/seller/components/SellerDashboard';
+import MyOrders from './features/seller/components/MyOrders';
+import MyIncome from './features/seller/components/MyIncome';
 
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/dashboard" element={<SellerDashboard />} />
+        <Route path="/orders" element={<MyOrders />} />
+        <Route path="/income" element={<MyIncome />} />
+        <Route path="/" element={<Navigate to="/login" replace />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
 
